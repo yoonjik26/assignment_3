d034033b307edea919cd7b7098d89ecf1c1e99cb
yoonjik26 <yoonjik1126@gmail.com>
2015-09-24 18:46:42 -0400

    sketch

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,18 +1,29 @@
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(640, 480);
 	noStroke();
-	noLoop();
+	smooth();
+	// noLoop();
+xpos = mouseX;
+ypos = mouseY;
 }
 
 function draw() {
 	// clear the background
-	background(150, 150, 50);
-
+	background(0, 0, 0);
+	xpos += (mouseX - xpos)/20;
+	ypos += (mouseY - ypos)/20;
+	//xpos = xpos + (mouseX - xpos)/5.0;
+	//ypos = ypos + (mouseY - ypos)/5.0;
 	// set a fill color
 	fill(255, 255, 255);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	var x = random(-5, 5);
+	if ((xpos == mouseX) && (ypos == mouseY)) {
+			console.log("test");
+			ellipse(xpos + x, ypos + x, 50, 50);
+	} else {
+		ellipse(xpos, ypos, 50, 50);
+	}
+
 }

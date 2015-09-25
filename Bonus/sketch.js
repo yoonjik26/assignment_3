8d58d54100dca2b4897bf10c4e86253d17fed79d
yoonjik26 <yoonjik1126@gmail.com>
2015-09-25 16:53:14 -0400

    sketch

--- Bonus/sketch.js
+++ Bonus/sketch.js
@@ -10,23 +10,16 @@ function draw() {
 
 colorMode(HSB);
 	for (var y = 0; y < 10; y++) {
-		if ( y === mouseY * 64){
+		for (var x = 0; x < 10; x++) {
+		if (floor(mouseX / 64) == x || floor(mouseY / 64) == y) {
 					fill(351.5, 95.45, random(0, 100));
 				} else {
 					fill(204, 0, random(0, 60));
 				}
-		for (var x = 0; x < 10; x++) {
-			if ( x === mouseX * 64){
-						fill(351.5, 95.45, random(0, 100));
-					} else {
-						fill(204, 0, random(0, 60));
-					}
+
 			rect(x * 64, y * 64, 64, 64);
 			// draw rectangle
 			fill(351.5, 95.45, random(0, 100));
-
-			rect(x * 64 , mouseY , 64, 64);
-			rect(mouseX, y * 64, 64, 64);
 			}
 	}
 }
